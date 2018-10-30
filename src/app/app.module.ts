2539cb769a78b1400302717b1620cf8742fc1e93
Jaime Diaz <jaime.diaz-gonzalez-external@capgemini.com>
2018-10-30 09:58:06 +0100

    #11 Correcting format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { HttpinterceptorProvider } from '../providers/security/httpinterceptor';
 import { AuthServiceProvider } from '../providers/security/auth-service';
-import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClient,
+  HttpClientModule,
+} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -18,27 +22,56 @@ import { SampledataDetail } from '../pages/sampledata-detail/sampledata-detail';
 import { SampledataRest } from '../providers/sampledata-rest';
 import { SampledataList } from '../pages/sampledata-list/sampledata-list';
 
-
 export function translateFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 @NgModule({
-    declarations: [MyApp, HomePage, LoginPage, HeaderComponent, SampledataList, SampledataDetail],
-    imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(MyApp), TranslateModule.forRoot({
-        loader: {
-            provide: TranslateLoader,
-            useFactory: translateFactory,
-            deps: [HttpClient]
-        }
-    })],
-    bootstrap: [IonicApp],
-    entryComponents: [MyApp, HomePage, LoginPage, SampledataList, SampledataDetail],
-    providers: [TranslateModule, StatusBar, SplashScreen, {
-        provide: ErrorHandler,
-        useClass: IonicErrorHandler
-    }, BusinessOperatorProvider, HttpClient, LoginProvider, AuthServiceProvider, {
-        provide: HTTP_INTERCEPTORS,
-        useClass: HttpinterceptorProvider,
-        multi: true
-    }, SampledataRest]
-}) export class AppModule {}
+  declarations: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    HeaderComponent,
+    SampledataList,
+    SampledataDetail,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(MyApp),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    SampledataList,
+    SampledataDetail,
+  ],
+  providers: [
+    TranslateModule,
+    StatusBar,
+    SplashScreen,
+    {
+      provide: ErrorHandler,
+      useClass: IonicErrorHandler,
+    },
+    BusinessOperatorProvider,
+    HttpClient,
+    LoginProvider,
+    AuthServiceProvider,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpinterceptorProvider,
+      multi: true,
+    },
+    SampledataRest,
+  ],
+})
+export class AppModule {}
