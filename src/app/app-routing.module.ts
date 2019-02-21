d1df65644cafbd7136d0d8a7a073540171cce89d
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2019-02-21 15:52:38 +0100

    Added ionicons and deleted login2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,7 @@ import { SampledataList } from './pages/sampledata-list/sampledata-list';
 const routes: Routes = [
   { path: '', loadChildren: './pages/home/home.module#HomePageModule', pathMatch: 'full' },
   { path: 'home', loadChildren: './pages/home/home.module#HomePageModule', pathMatch: 'full'},
-  { path: 'sampledata', component: SampledataList, pathMatch: 'full'},
-  { path: 'login2', loadChildren: './pages/login2/login2.module#Login2PageModule', pathMatch: 'full' }
-
+  { path: 'sampledata', component: SampledataList, pathMatch: 'full'}
 ];
 
 @NgModule({
