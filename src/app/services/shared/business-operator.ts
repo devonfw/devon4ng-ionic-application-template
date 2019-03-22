45e7ed503718b6a8bc8bb29ab839775a133d1faf
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2019-03-22 09:53:34 +0100

    [UPDATE] - Pixels to rem at login.page.scss

--- src/app/services/shared/business-operator.ts
+++ src/app/services/shared/business-operator.ts
@@ -6,15 +6,31 @@ export class BusinessOperatorProvider {
   public serverPath = SERVER_URL;
   public restPath = SERVER_URL + 'services/rest/';
   constructor() {}
+
+  /**
+  * @returns The url to the login service.
+  */
   login() {
     return this.restPath + 'login';
   }
+
+  /**
+  * @returns The url to the logout service.
+  */
   logout() {
     return this.restPath + 'logout';
   }
+
+  /**
+  * @returns The url to the csrf token service.
+  */
   getCsrf() {
     return this.restPath + 'security/v1/csrftoken';
   }
+
+  /**
+  * @returns The url to the sampledata management service.
+  */
   sampledataService() {
     return this.restPath + 'sampledatamanagement/v1/sampledata/';
   }
