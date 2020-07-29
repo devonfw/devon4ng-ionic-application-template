d4b1851008102369251a7cc84f788390b33efd05
Juan Andrés Ambuludi <juan-andres.ambuludi-olmedo@capgemini.com>
2020-07-29 11:45:07 +0200

    fixed tests

--- src/app/services/authorization/auth-guard.service.spec.ts
+++ src/app/services/authorization/auth-guard.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthGuardService } from './auth-guard.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AuthGuardService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule],
+    }),
+  );
 
   it('should be created', () => {
     const service: AuthGuardService = TestBed.inject(AuthGuardService);
